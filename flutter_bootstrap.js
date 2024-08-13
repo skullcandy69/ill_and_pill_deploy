e421231a82bba1d0a689859e55a0dd15612941a8
skullcandy69 <shiv.ahuja1494@gmail.com>
2024-08-13 20:10:48 +0530

    href set to repo name

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2147298582"
+    serviceWorkerVersion: "2739910529"
   }
 });
